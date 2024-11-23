b736faca43a63ab71f3bb6a77a5167278b87433d
Martin Ziegler <mz@v8w.de>
2024-11-23 11:31:12 +0100

    Changed hacs.json; added translation files

--- custom_components/better-trends/config_flow.py
+++ custom_components/better-trends/config_flow.py
@@ -1,27 +1,28 @@
 from homeassistant import config_entries
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
-from .const import DOMAIN, CONF_ENTITIES
+from .const import DOMAIN
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for the integration."""
+# Example schema for configuration input
+CONFIG_SCHEMA = vol.Schema(
+    {
+        vol.Optional("entities", default=[]): cv.multi_select(["sensor.example_1", "sensor.example_2"]),
+        vol.Optional("trend_values", default=5): vol.Coerce(int),
+        vol.Optional("interval", default=60): vol.Coerce(int),
+    }
+)
+
+class BetterTrendsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for BetterTrends."""
+
+    VERSION = 1
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is not None:
-            return self.async_create_entry(title="Trend Calculation", data=user_input)
+            return self.async_create_entry(title="BetterTrends", data=user_input)
 
-        available_entities = await self._get_available_entities()
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Optional(CONF_ENTITIES, default=[]): cv.multi_select(available_entities)
-            })
+            data_schema=CONFIG_SCHEMA
         )
-
-    async def _get_available_entities(self):
-        """Retrieve a list of all entities from Home Assistant."""
-        return {
-            entity_id: entity_id
-            for entity_id in self.hass.states.async_entity_ids()
-        }
