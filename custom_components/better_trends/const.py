792bef0f60fc4e7bea6b816967e8bd70af635d2f
Martin Ziegler <mz@v8w.de>
2024-12-07 10:54:50 +0100

    Changed default interval and steps

--- custom_components/better_trends/const.py
+++ custom_components/better_trends/const.py
@@ -1,7 +1,7 @@
 DOMAIN = "better_trends"
 CONF_ENTITIES = "entities"
-DEFAULT_INTERVAL = 5
-DEFAULT_TREND_VALUES = 5
+DEFAULT_INTERVAL = 12
+DEFAULT_TREND_VALUES = 10
 TREND_INTERVAL_ENTITY = "number.bettertrends_interval"
 TREND_VALUES_ENTITY = "number.bettertrends_steps"
 TREND_COUNTER_ENTITY = "number.bettertrends_current_step"
